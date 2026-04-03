9ff79f15770b4e7e4143ed7d75c4642a22e9ecfa
John Tyrone Pagunsan Coronel <jtcoronel.chmsu@gmail.com>
2026-04-03 13:38:29 +0800

    migrated

--- intake/forms.py
+++ intake/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import LandownerSubmission, ISFRecord
+from .models import LandownerSubmission, ISFRecord, HouseholdMember
 
 
 # Talisay City barangays (official 27 barangays)
